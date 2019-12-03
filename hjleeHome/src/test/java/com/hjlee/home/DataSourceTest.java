78a627f4e132181f05bf220eafc171d21613ca5d
hjlee <dlgudwo11@gmail.com>
2019-12-03 17:34:31 +0900

    mssql-jdbc 의존성 추가

--- hjleeHome/src/test/java/com/hjlee/home/DataSourceTest.java
+++ hjleeHome/src/test/java/com/hjlee/home/DataSourceTest.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 import javax.sql.DataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,11 +13,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/*.xml" })
 public class DataSourceTest {
 	@Inject
+	@Qualifier("dataSourceSpied")
 	private DataSource ds;
 
 	@Test
 	public void testConection() throws Exception {
 		try (Connection con = ds.getConnection()) {
+			System.out.println(ds.getClass());
 			System.out.println(con);
 		} catch (Exception e) {
 			e.printStackTrace();
